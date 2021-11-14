6eb3aeb4f18c28e5a3ffd088f54fa2cb6c5a1af9
Sébastien Schneider <sschneider@ausy.fr>
2021-11-14 15:17:13 +0100

    Add styling for form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -17,7 +17,7 @@ const Form = () => {
 
     return (
         <Paper className={classes.paper}>
-            <form autoComplete="off" novalidate className={classes.form} onSubmit={handleSubmit}>
+            <form autoComplete="off" novalidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant="h6">Creating a Memory</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth  value={postData.creator}onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth  value={postData.title}onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
