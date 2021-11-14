b54a4eda09eef2bbe15f8bec5eaff0c97310c22e
Sébastien Schneider <sschneider@ausy.fr>
2021-11-14 14:21:52 +0100

    Update with tutorial progress

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,7 @@
 export default (posts = [], action) => {
     switch (posts.type) {
         case 'FETCH_ALL':
-            return posts;
+            return action.payload;
          case 'CREATE':
             return posts;
         default:
